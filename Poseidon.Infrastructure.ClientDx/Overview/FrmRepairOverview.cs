bb2393df66ff23c2fd56f3c9db9df4e9fe1ecd38
robertzml <robertzml@126.com>
2018-09-02 17:18:27 +0800

    add FrmRepairOverview.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Infrastructure.ClientDx/Overview/FrmRepairOverview.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poseidon.Infrastructure.ClientDx
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Winform.Base;
+    using Poseidon.Infrastructure.Core.Utility;
+
+    /// <summary>
+    /// 维修改造总览
+    /// </summary>
+    public partial class FrmRepairOverview : BaseMdiForm
+    {
+        #region Constructor
+        public FrmRepairOverview()
+        {
+            InitializeComponent();
+        }
+        #endregion //Constructor
+    }
+}
